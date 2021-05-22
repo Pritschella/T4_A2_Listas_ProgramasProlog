e60e758030c176e5a299aa17d0fa950f4e48a698
Pritschella <prits99@hotmail.com>
2021-05-21 19:16:51 -0500

    Commit: "Se completó el ejercicio con el video 3 "

--- src/video2y3.pl
+++ src/video2y3.pl
@@ -3,3 +3,8 @@
 %Contar los elementos de una lista
 cuenta_elementos([], 0).
 cuenta_elementos([_|L], N):-cuenta_elementos(L, Tam), N is Tam +1.
+
+%https://www.youtube.com/watch?v=E6dC6ou7Nos
+%sumar los elementos de una lista
+suma_lista([], 0).
+suma_lista([X|L],N):-suma_lista(L,C), N is C+X.
